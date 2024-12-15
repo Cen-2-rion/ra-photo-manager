2a37bdae4d4744f2a5b6178b96bbde5b60ba5d03
Cen-2-rion <atrohov1982@gmail.com>
2024-12-15 21:02:02 +0300

    first commit ra-photo-manager(empty)

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Manager from './components/Manager';
+import './App.css';
+
+function App() {
+
+  return (
+    <Manager />
+  );
+}
+
+export default App;
---
+++ src/components/Manager.tsx
@@ -0,0 +1,4 @@
+const Manager = () => {
+}
+
+export default Manager;
